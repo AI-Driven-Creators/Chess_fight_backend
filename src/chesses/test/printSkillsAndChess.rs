b64b5ebd8c70e6b07fd02b11e077247f653907d3
yi0x44f <yi0x44f@gmail.com>
2025-05-04 18:40:56 +0800

    Add: printExample test

---
+++ src/chesses/test/printSkillsAndChess.rs
@@ -0,0 +1,18 @@
+// copy following code to main.rs
+
+
+// mod chesses;
+
+// use chesses::units::example::garen::GAREN_1;
+// use chesses::skills::example::fireball::FIREBALL;
+
+// #[tokio::main]
+// async fn main() {
+
+//     // Deref Lazy to get ChessTemplate
+//     println!("Chess: {}", GAREN_1.chess);
+//     // Deref Lazy to get Skill
+//     println!("Skill: {}", FIREBALL.name);
+
+
+// }
